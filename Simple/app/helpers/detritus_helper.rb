3e5fb5892b89547619eedfc5d0b513a964b46ff7
Gideon Providence <jprovidence@live.ca>
2011-11-16 17:59:26 -0500

    move simple to its own project

--- Simple/app/helpers/detritus_helper.rb
+++
@@ -1,2 +0,0 @@
-module DetritusHelper
-end
--- Simple/db/migrate/20111116011258_unique_index_on_detritus.rb
+++
@@ -1,8 +0,0 @@
-class UniqueIndexOnDetritus < ActiveRecord::Migration
-  def up
-    add_index :detritus, :link, :unique => true
-  end
-
-  def down
-  end
-end
